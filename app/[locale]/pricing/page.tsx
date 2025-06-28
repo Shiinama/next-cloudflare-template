812a70f72ca6b73f8e3abc284b7fe72a40343737
Felix <2072738187yu@gamil.com>
2025-06-28 22:52:09 +0800

    feat: pricing set

---
+++ app/[locale]/pricing/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+export default function PricingPage() {
+  return (
+    <div
+      className="h-96 w-80"
+      data-upgrade-chat-embed="2d7ea31d-bcb0-4062-b476-327bd6d7a8d6"
+      data-upgrade-chat-products="be66b10a-69dd-42f0-bf0d-97083b46344e"
+      data-upgrade-chat-baseurl="https://upgrade.chat"
+    />
+  )
+}
