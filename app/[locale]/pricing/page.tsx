b027eec4936efa5ef9aa66f938e4002860b8404b
Felix <2072738187yu@gamil.com>
2025-06-28 22:58:21 +0800

    feat: add payment

--- app/[locale]/pricing/page.tsx
+++ app/[locale]/pricing/page.tsx
@@ -2,11 +2,19 @@
 
 export default function PricingPage() {
   return (
-    <div
-      className="h-96 w-80"
-      data-upgrade-chat-embed="2d7ea31d-bcb0-4062-b476-327bd6d7a8d6"
-      data-upgrade-chat-products="be66b10a-69dd-42f0-bf0d-97083b46344e"
-      data-upgrade-chat-baseurl="https://upgrade.chat"
-    />
+    <div className="flex gap-4">
+      <div
+        className="h-96 w-80"
+        data-upgrade-chat-embed="2d7ea31d-bcb0-4062-b476-327bd6d7a8d6"
+        data-upgrade-chat-products="be66b10a-69dd-42f0-bf0d-97083b46344e"
+        data-upgrade-chat-baseurl="https://upgrade.chat"
+      />
+      <div
+        className="h-96 w-80"
+        data-upgrade-chat-embed="2d7ea31d-bcb0-4062-b476-327bd6d7a8d6"
+        data-upgrade-chat-products="62312764-a2ce-4e71-b585-0eefc55ec27d"
+        data-upgrade-chat-baseurl="https://upgrade.chat"
+      ></div>
+    </div>
   )
 }
