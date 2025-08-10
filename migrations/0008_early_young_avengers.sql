996f105549b8649d22ffcb02af609e030c1adc08
Felix <2072738187yu@gamil.com>
2025-08-11 00:35:26 +0800

    feat: add migrations

---
+++ migrations/0008_early_young_avengers.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `postTranslations` ADD `slug` text NOT NULL;--> statement-breakpoint
+ALTER TABLE `postTranslations` ADD `title` text NOT NULL;--> statement-breakpoint
+ALTER TABLE `postTranslations` ADD `excerpt` text NOT NULL;--> statement-breakpoint
+ALTER TABLE `postTranslations` ADD `cover_image_url` text;
