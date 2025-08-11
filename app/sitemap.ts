7d7a864afdbc8fbc13f5c1a18839c864686f145f
Felix <2072738187yu@gamil.com>
2025-08-11 23:50:15 +0800

    feat: i18n sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,35 +1,32 @@
-import { unstable_noStore } from 'next/cache'
-
 import { getAllArticles } from '@/actions/ai-content'
 import { locales } from '@/i18n/routing'
 
 import type { MetadataRoute } from 'next'
 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  unstable_noStore()
+export async function generateSitemaps() {
+  return locales.map((locale) => ({
+    id: locale.code
+  }))
+}
 
+export default async function sitemap({ id: locale }: { id: string }): Promise<MetadataRoute.Sitemap> {
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
 
-  const routes = ['', '/blogs']
+  const routes = ['', '/blogs', '/voice', '/voice-design', '/blogs']
 
-  // 为每个路由和每种语言创建sitemap条目
   const entries: MetadataRoute.Sitemap = []
 
   for (const route of routes) {
-    for (const locale of locales) {
-      entries.push({
-        url: `${baseUrl}${locale.code === 'en' ? '' : `/${locale.code}`}${route}`
-      })
-    }
+    entries.push({
+      url: `${baseUrl}${locale === 'en' ? '' : `/${locale}`}${route}`
+    })
   }
 
-  const allArticles = await getAllArticles()
+  const allArticles = await getAllArticles(locale)
 
-  const publishedArticles = allArticles
-    .filter((article) => article.publishedAt)
-    .map((i) => ({
-      url: `${baseUrl}/${!i.locale || i.locale === 'en' ? '' : i.locale + '/'}blog/${i.slug}`
-    }))
+  const publishedArticles = allArticles.map((article) => ({
+    url: `${baseUrl}${locale === 'en' ? '' : `/${locale}`}/blog/${article.slug}`
+  }))
 
   return [...entries, ...publishedArticles]
 }
