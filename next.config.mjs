f86d6670dd3ef7f81ab49a920aca0d7ac8696135
Felix <2072738187yu@gamil.com>
2025-12-08 22:58:07 +0800

    chore: change config to mjs

---
+++ next.config.mjs
@@ -0,0 +1,30 @@
+import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare'
+import createNextIntlPlugin from 'next-intl/plugin'
+
+initOpenNextCloudflareForDev()
+
+const withNextIntl = createNextIntlPlugin({
+  experimental: {
+    createMessagesDeclaration: './messages/en.json'
+  }
+})
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'standalone',
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: process.env.NEXT_PUBLIC_R2_DOMAIN,
+        port: '',
+        pathname: '/**'
+      },
+    ]
+  }
+}
+
+export default withNextIntl(nextConfig)
